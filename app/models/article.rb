4cbe08eb9a9b6df25e1527987a4745793bffaff9
Alex-JX <alexjx17@gmail.com>
2022-05-27 15:08:49 -0700

    Fixiign some bugs: validations and schema

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,11 @@
 class Article < ApplicationRecord
     include Visible
-    
+
     has_many :comments
 
     validates_presence_of :title, on: :create, message: "No puede estar vacio"
     validates_presence_of :body, on: :create, message: "No puede estar vacio"
-    validates :body, length: {minimum:2}, message: "No contener menos de 2 caracteres"
+    validates :body, length: {minimum:2, too_short: "Se requieren al menos de 2 caracteres"}
 
     
 end
