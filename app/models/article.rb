5acb910c381fae33766a9b569b8689a16a5ddbda
Alex-JX <alexjx17@gmail.com>
2022-05-27 13:45:27 -0700

    adding a new model: comment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+    has_many :comments
     validates_presence_of :title, on: :create, message: "No puede estar vacio"
     validates :body, presence: :true, length: {minimum:10}
 end
