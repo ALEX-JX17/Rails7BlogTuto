2930816362d8cd4f32712514e73496e36b9bcc8f
Alex-JX <alexjx17@gmail.com>
2022-05-27 15:37:56 -0700

    Added functionality to destroy comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,10 @@
 class Article < ApplicationRecord
     include Visible
 
-    has_many :comments
+    has_many :comments, dependent: :destroy
 
     validates_presence_of :title, on: :create, message: "No puede estar vacio"
     validates_presence_of :body, on: :create, message: "No puede estar vacio"
     validates :body, length: {minimum:2, too_short: "Se requieren al menos de 2 caracteres"}
-
-    
+ 
 end
