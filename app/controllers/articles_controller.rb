d02d1aa2241165234cca6fcd9382b17abde8d6a7
Alex-JX <alexjx17@gmail.com>
2022-06-05 14:40:06 -0700

    A litle change in the controllers's sintaxis

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -14,20 +15,18 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:success] = "Creado exitosamente"
+      flash[:success] = "Successfuly created"
       redirect_to @article
     else
-      flash[:error] = "Algo salio mal"
+      flash[:error] = "Something was wrong"
       render :new, status: :unprocessable_entity
     end
   end
 
   def edit
-    @article = Article.find(params[:id])
-  end
+     end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to @article
     else
@@ -36,14 +35,16 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-
+    @article.destroy 
     redirect_to articles_path, status: :see_other
     #redirect_back(fallback_location: articles_path)
   end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
         params.require(:article).permit(:title, :body, :status)
     end
