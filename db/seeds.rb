b4e6dadd7999e9013b55df11e7a09633446343d4
Alex-JX <alexjx17@gmail.com>
2022-06-09 19:07:47 -0700

    Seeds Added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# articles = Article.create([   
+#         {
+#             title: "Star Wars",
+#             body: "This is a popular movie",
+#             status: 'public'
+#         },{
+#             title: "The lord of the rings",
+#             body: "This is a very popular movie",
+#             status: 'public'
+#         }
+#     ])
+for i in 1..5
+    Article.create(
+        title: "Article Title #{i}", 
+        body: "Body for article #{i}", 
+        status: "public"
+        ).comments.create([{
+            commenter: "Commenter A", 
+            body: "Comment from commenter A for article #{i}", 
+            status: "public"},{
+            commenter: "Commenter B", 
+            body: "Comment from commenter B for article #{i}", 
+            status: "public" }])
+end
